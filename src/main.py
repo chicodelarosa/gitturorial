21e7a7e7f4adc7b96a00918dcbe07efbca5c2703
Dan A. Rosa De Jesus <dan.rosadejesus@pnnl.gov>
2022-04-20 10:36:58 -0700

    Add name modifier.

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 name = "Dev2"
 
+def namemodifier(str_):
+	return str_.upper()
+
 age = 15
 
-print(f"Hi my name is {name}. My age is {age}.")
+print(f"Hi my name is {namemodifier(name)}. My age is {age}.")
