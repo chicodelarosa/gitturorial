42c8ef67555e84ab008d5547c6a37284054894be
Dan A. Rosa De Jesus <dan.rosadejesus@pnnl.gov>
2022-04-20 08:59:23 -0700

    Setup.

--- src/main.py
+++ src/main.py
@@ -1,15 +1,5 @@
 name = "Dev2"
 
-age = 20
+age = 27
 
-def namemodifier(name):
-	newname = name + ' a string'
-
-	return newname
-
-def agemodifier(age):
-	newage = age - 2
-
-	return newage
-
-print(f"Hi my name is {namemodifier(name)}. My age is {agemodifier(age)}.")
+print(f"Hi my name is {name}. My age is {age}.")
