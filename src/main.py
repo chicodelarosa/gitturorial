956509c1586de371b6b77acbceb6cc33226ef806
Dan A. Rosa De Jesus <dan.rosadejesus@pnnl.gov>
2022-04-20 08:41:18 -0700

    age modifier

--- src/main.py
+++ src/main.py
@@ -1,6 +1,10 @@
 name = "Dev1"
 
+def agemodifier(age):
+	newage = age - 2
+
+	return newage
 
 age = 20
 
-print(f"Hi my name is {name}. My age is {age}.")
+print(f"Hi my name is {name}. My age is {agemodifier(age)}.")
